ca2bd78dece9e33604aaf08a68a32626b5346d45
Richard Sim <rdsim8589@gmail.com>
2017-01-06 01:30:23 +0000

    corrected for grammar

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,10 @@
 from sys import argv
 if __name__ == "__main__":
     num_arg = len(argv) - 1
-    if num_arg <= 2:
+    if num_arg == 1:
         print("{:d} argument:".format(num_arg))
+    elif num_arg <1:
+        print("{:d} argument.".format(num_arg))
     else:
         print("{:d} arguments:".format(num_arg))
     for i in range(1, len(argv)):
