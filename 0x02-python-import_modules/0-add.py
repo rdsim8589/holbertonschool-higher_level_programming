6123e46d7c2bcb94bbbb34e18d4a0b97ecfa9615
Richard Sim <rdsim8589@gmail.com>
2017-01-06 01:13:16 +0000

    completed assignments

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+from add_0 import add
+if __name__ == "__main__":
+    print("1 + 2 = {:d}".format(add(1, 2)))
---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from calculator_1 import sub, add, mul, div
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    signs = "+-x/"
+    SignByFun = {"-": sub, "+": add, "x": mul, "/": div}
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    if op in signs:
+        print("{:d} {} {:d} = {:d}".format(a, op, b, SignByFun[op](a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, x and *")
+        exit(1)
