4a997b199bbfd2721d68eeb4cbdccc7738dbace1
Richard Sim <rdsim8589@gmail.com>
2017-01-10 15:58:49 -0800

    removed shallow copy and made new copy

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    cp_list = my_list[:]
-    for i in range(len(cp_list)):
-        if cp_list[i] % 2 == 0:
-            cp_list[i] = True
+    cp_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            cp_list.append(True)
         else:
-            cp_list[i] = False
+            cp_list.append(False)
     return cp_list
