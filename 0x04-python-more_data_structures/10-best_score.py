9cc819ec46bd8b75cea3a48d84ba5441de8735f6
Richard Sim <rdsim8589@gmail.com>
2017-01-12 13:59:26 -0800

    corrected to use is not instead os !=

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict != None:
+    if my_dict is not None:
         for key in my_dict.keys():
             if my_dict[key] == max(list(my_dict.values())):
                 return key
