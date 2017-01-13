bd11fa08aced0c086441982570d20e03e0f2b81f
Richard Sim <rdsim8589@gmail.com>
2017-01-13 12:24:30 -0800

    changed exception into one line

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]),end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-        except TypeError:
-            pass
-        except ValueError:
+        except (TypeError, ValueError) as e:
             pass
     print("")
     return count
