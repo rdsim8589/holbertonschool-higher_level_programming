9b54f3631bdb95ba06264a718b6f45bdf0374c03
Richard Sim <rdsim8589@gmail.com>
2017-01-18 18:54:00 -0800

    moved div up first

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -13,14 +13,15 @@ def matrix_divided(matrix, div):
     """
     list_of_list = "matrix must be a matrix (list of lists) of integers/floats"
 
+    if not isinstance(div, int or float):
+        raise TypeError("div must be a number")
+
     if not isinstance(matrix, list) or matrix == []:
         raise TypeError(list_of_list)
 
     if not isinstance(matrix[0], list):
         raise TypeError(list_of_list)
 
-    if not isinstance(div, int or float):
-        raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
