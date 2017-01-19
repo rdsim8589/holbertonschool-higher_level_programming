4a76c218a9b8ed9e9a18d6438eda8c6d497f51e6
Richard Sim <rdsim8589@gmail.com>
2017-01-18 19:45:37 -0800

    added a condition to output something if size if greater than 0

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -15,4 +15,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    print("\n".join(["#" * size for i in range(size)]))
+    if size > 0:
+        print("\n".join(["#" * size for i in range(size)]))
