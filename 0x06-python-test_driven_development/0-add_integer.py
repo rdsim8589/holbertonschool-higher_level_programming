627ab58b276eb8addd838f0628ecd64d8eaab8b3
Richard Sim <rdsim8589@gmail.com>
2017-01-18 17:19:42 -0800

    make pep8 happy

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -8,12 +8,11 @@ to add two int or floats together
 
 
 def add_integer(a, b):
-   """
-   Return the addition of a and b
-   """
-   if not isinstance(a, (int, float)):
-       raise TypeError("a must be an integer")
-   if not isinstance(b, (int,float)):
-       raise TypeError("b must be an integer")
-
-   return int(a) + int(b)
+    """
+    Return the addition of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
