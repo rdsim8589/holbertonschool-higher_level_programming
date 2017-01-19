132d54452b49d90cfbd8b468782d6d7ffabd6aec
Richard Sim <rdsim8589@gmail.com>
2017-01-18 17:25:29 -0800

    made pep8 happy

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -11,8 +11,10 @@ def matrix_divided(matrix, div):
     """
     Returns a copy of the matrix divided by the int/float of div
     """
-    if not isinstance(matrix, list) or matrix == [] or not isinstance(matrix[0],list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    list_of_list = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(matrix, list) or matrix == []
+    or not isinstance(matrix[0], list):
+        raise TypeError(list_of_list)
     if not isinstance(div, int or float):
         raise TypeError("div must be a number")
     if div == 0:
@@ -22,13 +24,13 @@ def matrix_divided(matrix, div):
     cpy_mtx = []
     for row in matrix:
         if not isinstance(row, list) or row == []:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(list_of_list)
         if row_len != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         tmp_row = []
         for number in row:
             if type(number) != int and type(number) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(list_of_list)
             tmp_row.append(round(number/div, 2))
         cpy_mtx.append(tmp_row)
     return cpy_mtx
