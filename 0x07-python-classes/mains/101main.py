3745133e735fd1071a4556501480c6bb920ebf71
Richard Sim <rdsim8589@gmail.com>
2017-01-23 19:22:38 -0800

    moved all mains back to mains folder

---
+++ 0x07-python-classes/mains/101main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+Square = __import__('101-square').Square
+
+
+my_square = Square(5, (0, 3))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (3, 0))
+print(my_square)
+
+
+my_square = Square(5, (4, 4))
+print(my_square)
+
+print("--")
--- 0x07-python-classes/mains/4main.py
+++ 0x07-python-classes/mains/4main.py
@@ -12,3 +12,18 @@ try:
     print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 except Exception as e:
     print(e)
+try:
+    my_square = Square(-9)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
+
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
