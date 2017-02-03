2fd23a5d68e1757f1801402663384d24f1556cfa
Richard Sim <rdsim8589@gmail.com>
2017-02-03 12:13:36 -0800

    completed task 5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains funcion
+
+to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    returns the JSON representation of a object (string)
+    """
+    import json
+
+    return json.dumps(my_obj)
