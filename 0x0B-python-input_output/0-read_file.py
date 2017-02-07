a85c9ba8e40c61c8f2cc9820dd54a864f2de011a
rdsim8589 <rdsim8589@github.com>
2017-02-07 15:31:26 +0000

    removed the newline at the end of print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     read text file UTF8 and prints to stdout
     """
     with open(filename, encoding="utf-8") as myFile:
-        print("{}".format(myFile.read()))
+        print("{}".format(myFile.read()), end="")
