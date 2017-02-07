3042f2acdead2af2df45305815817afc5838e851
rdsim8589 <rdsim8589@github.com>
2017-02-07 15:32:20 +0000

    removed the import os

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -8,8 +8,6 @@ def number_of_lines(filename=""):
     """
     counts the number of lines in a file
     """
-    import os
-
     num_lines = 0
     with open(filename, encoding="utf-8") as myFile:
         return myFile.read().count('\n')
