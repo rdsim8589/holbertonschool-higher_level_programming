2366dcd81932d243a22459b2c466322419b34a44
Richard Sim <rdsim8589@gmail.com>
2017-02-22 23:34:16 -0800

    completed 100 to match proper formating

--- 0x0C-SQL_introduction/100-move_to_utf8.sql
+++ 0x0C-SQL_introduction/100-move_to_utf8.sql
@@ -6,5 +6,12 @@ ALTER DATABASE
 
 ALTER TABLE
       hbtn_0c_0.first_table
-      CONVERT CHARACTER SET = utf8mb4
-      COLLATE = utf8mb4_unicode_ci;
+      CONVERT TO CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+      hbtn_0c_0.first_table
+      CHANGE name name
+      VARCHAR(256)
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
