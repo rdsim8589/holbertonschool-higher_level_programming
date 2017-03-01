dc6bceecfcca5d779eb5c47662eadb1f45f9001b
Richard Sim <rdsim8589@gmail.com>
2017-02-28 17:13:33 -0800

    made name lower case

--- 0x0D-SQL_more_queries/5-unique_id.sql
+++ 0x0D-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- a script that creates the table unique_id on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE,
-       NAME VARCHAR(256));
+       name VARCHAR(256));
