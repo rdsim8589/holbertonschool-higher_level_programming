4e54e69e3a4930ff7c7408a206059de7d8ad70cd
Richard Sim <rdsim8589@gmail.com>
2017-03-01 00:08:19 -0800

    correct spacing

--- 0x0D-SQL_more_queries/15-comedy_only.sql
+++ 0x0D-SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title
        FROM tv_show_genres
        	    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-       	    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-	    WHERE tv_genres.name = 'Comedy'
-	ORDER BY tv_shows.title ASC
+	    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+       WHERE tv_genres.name = 'Comedy'
+       ORDER BY tv_shows.title ASC
