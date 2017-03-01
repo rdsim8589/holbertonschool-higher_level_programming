aafadc8ca110ea862f1cf64bcd9d16b238028623
Richard Sim <rdsim8589@gmail.com>
2017-02-28 16:47:51 -0800

    updated to create user properly

--- 0x0D-SQL_more_queries/2-create_read_user.sql
+++ 0x0D-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 --  script that creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
-GRANT ALL PRIVILEGES ON htbn_0d_2. * TO 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES
