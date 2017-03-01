c6fbaf7e34d3c0074d6e21bb06c12876889f9e79
Richard Sim <rdsim8589@gmail.com>
2017-03-01 00:05:26 -0800

    removed the second asc

--- 0x0D-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0D-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_genres.name
        FROM tv_show_genres
        	    RIGHT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 	    RIGHT OUTER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-       ORDER BY tv_shows.title ASC , tv_genres.name ASC;
+       ORDER BY tv_shows.title ASC;
