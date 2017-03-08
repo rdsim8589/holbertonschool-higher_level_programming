2539119952b8b7dded4b2c803de3a1f34b66f282
Richard Sim <rdsim8589@gmail.com>
2017-03-08 15:45:11 -0800

    added the port

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
     try:
         db = MySQLdb.connect(host="localhost",
+                             port=3306,
                              user=argv[1],
                              passwd=argv[2],
                              db=argv[3])
