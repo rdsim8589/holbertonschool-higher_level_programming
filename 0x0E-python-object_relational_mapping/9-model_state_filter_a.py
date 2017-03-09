c15cbfc64a47cdbe1f3ab013b99eb8a23f3959df
Richard Sim <rdsim8589@gmail.com>
2017-03-08 19:26:40 -0800

    find all states with n in it

--- 0x0E-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0E-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+find all states with 'n' in it
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id).filter(
+            State.name.like('%n%')):
+        print("{}: {}".format(instance.id, instance.name))
