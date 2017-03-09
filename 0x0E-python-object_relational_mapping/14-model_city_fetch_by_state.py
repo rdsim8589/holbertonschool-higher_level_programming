a87e9549bb40bb7bf1fcc59f108294dabc80c9b7
Richard Sim <rdsim8589@gmail.com>
2017-03-08 22:21:55 -0800

    join two tables and select columns state:name, city:id, city:name

--- 0x0E-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0E-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+find all states with 'a' in it
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city, state in session.query(
+            City, State).join(State).order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
