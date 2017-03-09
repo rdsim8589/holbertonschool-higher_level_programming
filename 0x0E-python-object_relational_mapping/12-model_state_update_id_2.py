b02db7a6368b97c27dd1ad052c76ae40c4a55fde
Richard Sim <rdsim8589@gmail.com>
2017-03-08 20:49:32 -0800

    script to change state name for id 2

--- 0x0E-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0E-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+find all states with 'a' in it
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State).filter_by(id=2):
+        row.name = "New Mexico"
+    session.commit()
