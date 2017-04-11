04e97b703239fa117fd63e2f9107e3d83b821044
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:41:40 -0700

    replaced for tab

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("""Body response:
-        - type: {}
-        - content: {}
-        - utf8 content: {:s}""".format(type(html),
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {:s}""".format(type(html),
                                        html,
                                        html.decode(encoding='UTF-8')))
