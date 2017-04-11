8480d69b57d7f7f6f16fe7355dd645e81462a511
Richard Sim <rdsim8589@gmail.com>
2017-04-11 14:05:05 -0700

    python script that sends a url resquest and gets a response. It catches and displays Error code

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL, sends request to the URL
+and displays the body of the response
+"""
+import sys
+import urllib.request
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(req)
+        print(response.read().decode(encoding='UTF-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
