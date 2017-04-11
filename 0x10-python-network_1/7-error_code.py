606726bb814554d74661ab1b44f8b3a482a816fd
Richard Sim <rdsim8589@gmail.com>
2017-04-11 15:07:12 -0700

    created python script that sends a requests and displays the body of the response if response code is less than 400

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+modules that contains a script to take in a URL send a request.
+It than displays the body response
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    status_code = r.status_code
+    if (status_code < 400):
+        print(r.text)
+    else:
+        print("Error code: {:d}".format(status_code))
