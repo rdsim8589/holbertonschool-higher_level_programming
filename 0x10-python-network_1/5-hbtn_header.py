eb76900bf81d43441035bb535fc3138573a23f8a
Richard Sim <rdsim8589@gmail.com>
2017-04-11 14:35:50 -0700

    python script that takes a URL, sends a URL request and displays the X-Request-Id to standard out

--- 0x10-python-network_1/5-hbtn_header.py
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This module takes in a URL, sends a request to the URL,
+and displays the value of the X-Request-Id
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
+    except Exceptions as e:
+        print(e)
