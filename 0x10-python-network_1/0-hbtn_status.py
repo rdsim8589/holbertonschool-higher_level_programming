4601210340c3436382a3c1d86017edc89bc15a05
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:19:08 -0700

    account for using tabs

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -5,12 +5,14 @@ hbtn_status module
 Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
+
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("""Body reponse:
-        - type: {}
-        - content: {}
-        - utf8 content: {:s}""".format(type(html),
+/t- type: {}
+/t- content: {}
+/t- utf8 content: {:s}""".format(type(html),
                                        html,
                                        html.decode(encoding='UTF-8')))
