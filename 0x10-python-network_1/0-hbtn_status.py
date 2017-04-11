e02b8802e8bee76a688ecfa52b2f65bd865f5a32
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:40:06 -0700

    added the response

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("""Body:
+        print("""Body response:
         - type: {}
         - content: {}
         - utf8 content: {:s}""".format(type(html),
