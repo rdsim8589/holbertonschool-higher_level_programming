e589eadf3efc19eb9138d759a5fd84c66c5b9bc2
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:26:58 -0700

    spelled reponse wrong

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -10,9 +10,9 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body reponse:\n"
-              "\t- type: {}\n"
-              "\t- content: {}\n"
-              "\t- utf8 content: {:s}".format(type(html),
+        print("""Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {:s}""".format(type(html),
                                        html,
-                                       html.decode('UTF-8')))
+                                       html.decode(encoding = 'UTF-8')))
