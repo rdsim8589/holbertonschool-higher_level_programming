3398781eaefbe528df88780b892778772f6e6c57
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:23:33 -0700

    removed the doc string

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -10,9 +10,9 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("""Body reponse:
-        - type: {}
-        - content: {}
-        - utf8 content: {:s}""".format(type(html),
+        print("Body reponse:\n"
+              "\t- type: {}\n"
+              "\t- content: {}\n"
+              "\t- utf8 content: {:s}".format(type(html),
                                        html,
                                        html.decode(encoding='UTF-8')))
