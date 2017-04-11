a9a72ed71ac97a9b840b6cb04cf76ce744840590
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:46:50 -0700

    changed to only import sys

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -4,11 +4,11 @@ This module takes in a URL, sends a request to the URL,
 and displays the value of the X-Request-Id
 """
 import urllib.request
-from sys import argv
+import sys
 if __name__ == "__main__":
     try:
         # urllib.request.urlopen(<url>) returns a HTTPResponse Object
-        with urllib.request.urlopen(argv[1]) as request:
+        with urllib.request.urlopen(sys.argv[1]) as request:
             # info returns a dictionary of the HTTP header
             print(request.info()['X-Request-Id'])
     except Exception as e:
