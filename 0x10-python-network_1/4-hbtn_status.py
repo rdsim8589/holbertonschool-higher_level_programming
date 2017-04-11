2bde1cabb48af61e45512c000f745a791fe48c9c
Richard Sim <rdsim8589@gmail.com>
2017-04-11 14:19:39 -0700

    made pep8 happy

--- 0x10-python-network_1/4-hbtn_status.py
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -5,11 +5,11 @@ module that fetches https://intranet.hbtn.io/status
 Using the requests package
 """
 import requests
-if __name__=="__main__":
+if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("""Body response:
 \t- type: {}
 \t- content: {}
 \t- utf8 content: {:s}""".format(type(r.content),
-                                  r.content,
-                                  r.text))
+                                 r.content,
+                                 r.text))
