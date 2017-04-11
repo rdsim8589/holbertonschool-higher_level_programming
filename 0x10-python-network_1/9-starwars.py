0086b4ef97ad5653fda5d2e03f75d6e95105c599
Richard Sim <rdsim8589@gmail.com>
2017-04-11 15:59:54 -0700

    completed a script that uses starwars api

--- 0x10-python-network_1/9-starwars.py
+++ 0x10-python-network_1/9-starwars.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Module that takes in a string and sends request to Star Wards API
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    try:
+        url = "https://swapi.co/api/people/?search={}".format(sys.argv[1])
+        r = requests.get(url)
+        print("Number of results: {}".format(r.json()["count"]))
+        for search_result in r.json().get("results"):
+            print(search_result.get("name"))
+    except IndexError:
+        print("pass search parameter")
