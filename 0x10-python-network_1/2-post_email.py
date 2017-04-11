42a43a9d5c0a783b61953c05bb25e82545cb4503
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:48:10 -0700

    change to import sys

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -4,12 +4,12 @@ module that takes in an URL and email, sends a POST request
 to the passed URL with the email as a parameter , and displays
 the body of the response (decode in utf-8)
 """
-from sys import argv
+import sys
 import urllib.parse
 import urllib.request
 if __name__ == "__main__":
-    url = argv[1]
-    values = {'email': argv[2]}
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
     # urlencode converts a mapping obj to str or bits
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
