c81ecaffcf7d1ee36f90c6f7360130d257f9909e
Richard Sim <rdsim8589@gmail.com>
2017-04-11 15:41:04 -0700

    completed python script that checks if json is properly formated

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+Module that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with a letter as a parameter
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        q = sys.argv[1]
+    except IndexError:
+        q = ""
+    data = {'q': q}
+    r = requests.post(url, data=data)
+    try:
+        id_ = r.json()['id']
+        name_ = r.json()['name']
+        print("[{}] {}".format(id_, name_))
+    except ValueError:
+        print("Not a Valid Json")
+    except KeyError:
+        print("No result")
