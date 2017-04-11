8a5af1b5043d10649b95a1de43759b454f7099c3
Richard Sim <rdsim8589@gmail.com>
2017-04-11 11:23:30 -0700

    created python script that fetches https://intranet.hbtn.io/status

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+hbtn_status module
+
+Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+if __name__ == "__main__":
+#    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("""Body reponse:
+        - type: {}
+        - content: {}
+        - utf8 content: {:s}""".format(type(html),html, html.decode(encoding='UTF-8')))
