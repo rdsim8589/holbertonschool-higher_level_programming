ac5ca8d454f5b904029e30659b25de0759c60dd1
Richard Sim <rdsim8589@gmail.com>
2017-04-11 13:31:32 -0700

    made pep8 happy

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -5,8 +5,6 @@ hbtn_status module
 Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
-
-
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
