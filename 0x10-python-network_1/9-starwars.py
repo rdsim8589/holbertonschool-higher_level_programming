50b2ac9ef5326610128e5d5e0d1d45b3af49ae2a
Richard Sim <rdsim8589@gmail.com>
2017-04-11 16:03:40 -0700

    changed results to result

--- 0x10-python-network_1/9-starwars.py
+++ 0x10-python-network_1/9-starwars.py
@@ -6,9 +6,9 @@ import sys
 import requests
 if __name__ == "__main__":
     try:
-        url = "https://swapi.co/api/people/?search={}".format(sys.argv[1])
+        url = "https://swapi.co/api/people/?search={:s}".format(sys.argv[1])
         r = requests.get(url)
-        print("Number of results: {}".format(r.json()["count"]))
+        print("Number of result: {}".format(r.json()["count"]))
         for search_result in r.json().get("results"):
             print(search_result.get("name"))
     except:
