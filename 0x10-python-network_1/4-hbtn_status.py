2a9abbb673caa101afc02f3e438e493f6add094a
Richard Sim <rdsim8589@gmail.com>
2017-04-11 14:17:25 -0700

    completed 4 with request

--- 0x10-python-network_1/4-hbtn_status.py
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+module that fetches https://intranet.hbtn.io/status
+
+Using the requests package
+"""
+import requests
+if __name__=="__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("""Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {:s}""".format(type(r.content),
+                                  r.content,
+                                  r.text))
