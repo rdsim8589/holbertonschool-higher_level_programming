b1dfd453da699462c1768183595f5b494bc53aad
Richard Sim <rdsim8589@gmail.com>
2017-04-11 14:59:24 -0700

    script that sends a request with URl and email address and gets back a display of the body reponse"

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+module to take in the URl and email,
+sends a POST request
+passes the URL as a parameter
+finally displays the header
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    r = requests.post(url, data=data)
+    print(r.text)
