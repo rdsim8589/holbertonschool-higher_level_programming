629eb5d9ec97996a0278093f83679a1b2216f79c
Richard Sim <rdsim8589@gmail.com>
2017-04-10 20:23:59 -0700

    changed to equal size

--- 0x0F-python-network_0/41-post_params.sh
+++ 0x0F-python-network_0/41-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a post request and sets variables. returns the body as response
-curl -X POST -d "email: hr@holbertonschool.com" -d "subject: I will always be here for PLD" $1 
+curl -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" $1 
