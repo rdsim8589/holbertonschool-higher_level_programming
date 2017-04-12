0532e47f49b48741a660bc70915342bd91fa5e43
Richard Sim <rdsim8589@gmail.com>
2017-04-11 17:03:12 -0700

    completed getting id using github api

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Script that takes Github credenials,
+displays Id using Github API
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    try:
+        user = sys.argv[1]
+        password = sys.argv[2]
+        r = requests.get("https://api.github.com/user", auth=(user, password))
+        print(r.json().get('id'))
+    except IndexError:
+        print("give username and password")
