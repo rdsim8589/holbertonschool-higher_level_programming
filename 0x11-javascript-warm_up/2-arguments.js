b9ffa147b23e76500de10ceb9ed790ef07de60a9
Richard Sim <rdsim8589@gmail.com>
2017-04-27 16:59:19 -0700

    change to the use of slice

--- 0x11-javascript-warm_up/2-arguments.js
+++ 0x11-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const numberArgs = process.argv.length - 3;
-if (numberArgs < 0) {
+const numberArgs = process.argv.slice(2).length;
+if (numberArgs === 0) {
   console.log('No argument');
-} else if (numberArgs === 0) {
+} else if (numberArgs === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
