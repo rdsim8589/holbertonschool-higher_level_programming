3416d323457045ce793dc423980c1dee1012358f
Richard Sim <rdsim8589@gmail.com>
2017-04-27 16:51:29 -0700

    return appropreiate string based on arguements passed

--- 0x11-javascript-warm_up/2-arguments.js
+++ 0x11-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const numberArgs = process.argv.length - 3;
+if (numberArgs < 0) {
+  console.log('No Arguement');
+} else if (numberArgs === 0) {
+  console.log('Arguement found');
+} else {
+  console.log('Arguements found');
+}
