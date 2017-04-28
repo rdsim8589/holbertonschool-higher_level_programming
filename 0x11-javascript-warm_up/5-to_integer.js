cd36b8b256776430538011bd632604ab810bb859
Richard Sim <rdsim8589@gmail.com>
2017-04-27 17:41:26 -0700

    takes a argument and determines if it is a number

--- 0x11-javascript-warm_up/5-to_integer.js
+++ 0x11-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const number = parseInt(args[0]);
+if (args.length === 0 || isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
+}
