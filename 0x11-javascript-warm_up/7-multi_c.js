2b87fff4b3c3f6037bd131953420db781dffdff2
Richard Sim <rdsim8589@gmail.com>
2017-04-27 19:10:53 -0700

    testing for isNaN cause that would account for len as well

--- 0x11-javascript-warm_up/7-multi_c.js
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const argsLen = args.length;
 const num = parseInt(args[0]);
-if (argsLen > 0 && !isNaN(num)) {
+if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
