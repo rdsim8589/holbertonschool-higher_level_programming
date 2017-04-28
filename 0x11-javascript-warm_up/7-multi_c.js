0b07ff118332493c637e774fe65086633de5b05c
Richard Sim <rdsim8589@gmail.com>
2017-04-27 17:52:38 -0700

    print 'C is fun' the number pasted in the arguments

--- 0x11-javascript-warm_up/7-multi_c.js
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const argsLen = args.length;
+const num = parseInt(args[0]);
+if (argsLen > 0 && !isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurances');
+}
