b4ec02aa75476e26615fe3fd052496deabec04b2
Richard Sim <rdsim8589@gmail.com>
2017-04-27 18:04:44 -0700

    create a square with argument given as input

--- 0x11-javascript-warm_up/8-square.js
+++ 0x11-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const num = parseInt(process.argv.slice(2)[0]);
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('x'.repeat(num));
+  }
+} else {
+  console.log('Missing size');
+}
