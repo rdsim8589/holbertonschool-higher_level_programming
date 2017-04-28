d79461973a048474eb48b0c65998c982ff4afb87
Richard Sim <rdsim8589@gmail.com>
2017-04-27 18:19:11 -0700

    print factorial of given user argument

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let total = 1;
+let num = parseInt(process.argv.slice(2)[0]);
+if (isNaN(num)) {
+  num = 1;
+}
+while (num > 0) {
+  total *= num;
+  num--;
+}
+console.log(total);
