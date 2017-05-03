ab082d24931d0666c1266c29ae5761856e1cda3d
Richard Sim <rdsim8589@gmail.com>
2017-05-02 17:26:29 -0700

    completed extra credit

---
+++ 0x13-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', theDomHasLoaded, false);
+function theDomHasLoaded (e) {
+  let header = document.querySelector('HEADER');
+  header.style.color = '#FF0000';
+}
