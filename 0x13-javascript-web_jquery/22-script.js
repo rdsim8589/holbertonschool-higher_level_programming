aa81e00be4641db9aece2422286ab8f6381d815a
Richard Sim <rdsim8589@gmail.com>
2017-05-02 21:51:17 -0700

    made semistandard happy

--- 0x13-javascript-web_jquery/22-script.js
+++ 0x13-javascript-web_jquery/22-script.js
@@ -1,4 +1,4 @@
-$.get('http://swapi.co/api/films/?format=json', function(data) {
+$.get('http://swapi.co/api/films/?format=json', function (data) {
   const count = data.count;
   let i = 0;
   while (i < count) {
